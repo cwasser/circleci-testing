da29b8e0d017363cd0da3cbc15f48bafa8c9999c
Klea Sanka <klea.sanka@trivago.com>
2017-07-26 13:35:16 +0200

    First complete version

--- web-testing/features/bootstrap/FeatureContext.php
+++ web-testing/features/bootstrap/FeatureContext.php
@@ -6,11 +6,15 @@ use Behat\Behat\Context\Context;
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context  {
+    private $host;
+
     /** @When I call :host */
-    public function iCall(string $host, string $port) :void {
+    public function iCall(string $host) :void {
+        $this->host = $host;
+
         $timeout   = 5.00;
         $timeStart = microtime(true);
-        $connection = @fsockopen($host, $port);
+        $connection = @fsockopen($host, 80);
 
         while ((microtime(true) - $timeStart) < $timeout) {
             if ($connection === true) {
@@ -22,7 +26,9 @@ class FeatureContext implements Context  {
     }
 
     /** @Then I should get a :expectedBody response */
-    public function iShouldGetAResponseWhenICall(string $expectedBody, string $url): void {
+    public function iShouldGetAResponseWhenICall(string $expectedBody): void {
+        $url = sprintf('http://%s/hello-world', $this->host);
+
         $responseBody = file_get_contents($url);
         assert($responseBody === $expectedBody);
     }
--- web-testing/features/bootstrap/HelloWorld.feature
+++ web-testing/features/bootstrap/HelloWorld.feature
@@ -2,5 +2,5 @@
 Feature: API is able to respond with "Hello, World!"
 
   Scenario: Get "Hello, World!" response from API
-    When I call 'http://api.fleshgrinder.docker/hello-world'
+    When I call 'api.fleshgrinder.docker'
     Then I should get a '"Hello, World!"' response
